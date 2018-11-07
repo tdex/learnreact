18702bc59a75025fed3482c2eec304828b401a5e
Tarcísio Deschamps Silva <tardchamps94@gmail.com>
2018-11-06 22:22:13 -0300

    Olá mundo React

---
+++ src/js/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let App = (
+  <div className='container'>
+    <h1>Olá mundo!!</h1>
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById('app'));
